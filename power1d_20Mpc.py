2ecf2dda48e00a784f96ccaf8e9115d488cfa3e3
andersdot <l.sonofanders@gmail.com>
2018-07-17 00:38:17 -0700

    power 1d spectra

---
+++ power1d_20Mpc.py
@@ -0,0 +1,96 @@
+import numpy as np
+import astropy.units as u
+import math as mh
+import sys
+
+import power_spectra as spe
+import boxes as box
+import fourier_estimators as fou
+import utils as uti
+
+def get_k_bin_edges_logspace(n_k_bins, k_box):
+    k_max = np.max(k_box) #0.704 / u.Mpc
+
+    k_min = np.min(k_box[k_box > 0. / u.Mpc])
+    k_bin_max = mh.exp(mh.log(k_max.value) + ((mh.log(k_max.value) - mh.log(k_min.value)) / (n_k_bins - 1))) / u.Mpc
+    return np.exp(np.linspace(mh.log(k_min.value), mh.log(k_bin_max.value), n_k_bins + 1)) / u.Mpc
+
+def get_mu_bin_edges_linspace(n_mu_bins):
+    return np.linspace(0., 1., n_mu_bins + 1)
+
+
+if __name__ == "__main__":
+    """Input arguments: Snapshot number; Snapshot directory path; Width of skewer grid in samples;
+    Resolution of spectra in km s^{-1}; Spectra directory path (with '/snapdir_XXX' if necessary)"""
+
+
+import boxes
+import numpy as np
+import astropy.units as u
+import math as mh
+import sys
+import power_spectra as spe
+import fourier_estimators as fou
+import boxes
+import bigfile
+import subprocess
+import os.path
+import matplotlib as mpl
+mpl.use('pdf')
+import matplotlib.pyplot as plt
+
+boxsize = 20. #Mpc/h
+grid_width = 200
+spectral_res = 10*u.km/u.s
+n = 50
+snapshot_dir_pre = '/home/fvillaescusa/data/Lya_ncv/20Mpc_256/'
+spectra_dir_pre = '/home/landerson/lyalpha/'
+sim_pres = ['', 'NCV_0_', 'NCV_1_']
+
+alphas = [0.4, 0.6, 0.8, 1.0]
+simulation_numbers = np.arange(0, n)
+colors = mpl.cm.Blues(np.linspace(0.5, 1.0, n))
+snapshot_numbers = [0, 1,2]
+
+for sim_pre in sim_pres:
+
+    for snapshot_number in snapshot_numbers:
+
+        snapshot_num = snapshot_number
+        p1 = []
+
+        k1 = []
+
+        for simulation_number, c in zip(simulation_numbers, colors):
+            if(simulation_number > 24) & (sim_pre == 'NCV_0_'):continue
+            if(simulation_number > 24) & (sim_pre == 'NCV_1_'):continue
+
+            snapshot_directory = snapshot_dir_pre + sim_pre + str(simulation_number)
+            save_directory = spectra_dir_pre + sim_pre + str(simulation_number)
+            spectra_directory = save_directory + '/SPECTRA_{0:03d}'.format(snapshot_number)
+            print(spectra_directory, snapshot_directory)
+            simulation_box_instance = boxes.SimulationBox(snapshot_num, snapshot_directory, grid_width, spectral_res,reload_snapshot=False, spectra_savedir=spectra_directory)
+
+            simulation_box_instance.convert_fourier_units_to_distance = True
+            delta_flux_box = simulation_box_instance.skewers_realisation()
+
+            fourier_estimator_instance = fou.FourierEstimator1D(delta_flux_box)
+            power = fourier_estimator_instance.get_power_1D()
+            k = simulation_box_instance.k_i('z').value
+            #np.savez(SPECTRA_SAVEDIR + POWER_SPECTRA_SAVEFILE, power_binned, k_binned, mu_binned, bin_counts)
+
+            p1.append(power)
+            k1.append(k)
+            #plt.plot(np.log10(k), np.log10(power), color=c, label=None)
+            #plt.xlabel('k [h/Mpc]')
+            #plt.ylabel('P(k)')
+            #plt.tight_layout()
+            #plt.savefig('test{0}.pdf'.format(sim_pre))
+            #import pdb; pdb.set_trace()
+        np.savez('spec1d{0}_{1}{2}_1mubin_20Mpc'.format(grid_width, sim_pre, snapshot_number), power=p1, k=k1)
+
+    #plt.legend(loc='best')
+    #plt.xlabel('k [h/Mpc]')
+    #plt.ylabel('P(k)')
+    #plt.tight_layout()
+    #plt.savefig('test1d{0}.pdf'.format(sim_pre))
