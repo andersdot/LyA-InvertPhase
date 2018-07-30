cfca24ab57cda5259402bb25478222b9243fb76f
salomanders <landerson@simonsfoundation.org>
2018-07-30 17:34:05 +0200

    pretty paper plots

--- paperPlots.py
+++ paperPlots.py
@@ -12,8 +12,8 @@ def lnMeanFlux(z):
 def rms(x):
     np.sqrt(np.sum(x**2.)/len(x))
 
-def plotAllPS(power, kArray, ax, color='black', alpha=0.1, label=None):
-    for i, (p, k) in enumerate(zip(power, kArray)):
+def plotAllPS(power, k, ax, color='black', alpha=0.1, label=None):
+    for i, p, in enumerate(power):
         if i == 0: label = label
         else: label = None
         ax.loglog(k, p, color=color, lw=0.5, alpha=alpha, label=label)
@@ -53,7 +53,9 @@ ylim_avg = (0.3, 2)
 savenpz_pre = ['T', 'NCV_0', 'NCV_1']
 snap_pre = ['', 'NCV_0_', 'NCV_1_']
 
-bigbox = True
+bigbox = False
+dimension = '3d'
+
 
 if bigbox:
     grid_width = 400
@@ -64,7 +66,14 @@ else:
     boxsize = 20
     filenamePostfix = '_1mubin_20Mpc.npz'
 
-fixK = 2.
+
+if dimension == '1d':
+    normalization = boxsize
+    plotPostFix = ''
+else:
+    normalization = boxsize**3.
+    plotPostFix = '_1mubin'
+
 
 alpha_line = 0.75
 alpha_fill = 0.75
@@ -78,18 +87,25 @@ for sn, z, c in zip(snap_nums, zz, colors):
     fig.subplots_adjust(hspace=0)
     plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
 
-    dataT = np.load('/Users/landerson/LyA-InvertPhase/goodspec/spec3d{0}_{1}'.format(grid_width, sn) + filenamePostfix)
-    data0 = np.load('/Users/landerson/LyA-InvertPhase/goodspec/spec3d{0}_NCV_0_{1}'.format(grid_width, sn) + filenamePostfix)
-    data1 = np.load('/Users/landerson/LyA-InvertPhase/goodspec/spec3d{0}_NCV_1_{1}'.format(grid_width, sn) + filenamePostfix)
+    dataT = np.load('/Users/landerson/LyA-InvertPhase/goodspec{0}/spec{0}{1}_{2}'.format(dimension, grid_width, sn) + filenamePostfix)
+    data0 = np.load('/Users/landerson/LyA-InvertPhase/goodspec{0}/spec{0}{1}_NCV_0_{2}'.format(dimension, grid_width, sn) + filenamePostfix)
+    data1 = np.load('/Users/landerson/LyA-InvertPhase/goodspec{0}/spec{0}{1}_NCV_1_{2}'.format(dimension, grid_width, sn) + filenamePostfix)
 
     shapeT = dataT['power'].shape
-    powerT = dataT['power'].reshape(shapeT[0:2])*boxsize**3.
-    kT = dataT['k'].reshape(shapeT[0:2])
+    powerT = dataT['power'].reshape(shapeT[0:2])*normalization
     nTrad = shapeT[0]
+    if dimension == '1d':
+        kT = np.arange(shapeT[1])[np.newaxis, :]*2*np.pi/boxsize
+    else:
+        kT = dataT['k'].reshape(shapeT[0:2])
+
 
     shapeP = data0['power'].shape
-    powerP = 0.5*(data0['power'].reshape(shapeP[0:2]) + data1['power'].reshape(shapeP[0:2]))*boxsize**3.
-    kP = data0['k'].reshape(shapeP[0:2])
+    powerP = 0.5*(data0['power'].reshape(shapeP[0:2]) + data1['power'].reshape(shapeP[0:2]))*normalization
+    if dimension == '1d':
+        kP = np.arange(shapeP[1])[np.newaxis, :]*2*np.pi/boxsize
+    else:
+        kP = data0['k'].reshape(shapeP[0:2])
     nPaired = shapeP[0]
 
     print(nTrad, nPaired)
@@ -102,23 +118,26 @@ for sn, z, c in zip(snap_nums, zz, colors):
 
 
     #import pdb; pdb.set_trace()
-    uncert = 2.*varT/(nTrad*varP)
+    uncert = 1/2.*np.sqrt(varT/varP)*np.sqrt(2./nTrad + 2./nPaired)
     yp = varT/varP + uncert
     ym = varT/varP - uncert
+    print(yp, ym)
     ax[2].semilogx(kT[0], varT/varP, color=c, lw=2)
-    ax[2].fill_between(kT[0], ym,  yp, color=c, alpha=0.5)
+    ax[2].fill_between(kT[0], ym,  yp, color=c, alpha=0.75)
     #ax[2].fill_between(kT[0][:,0], ym[:,0],  yp[:,0], color=c, alpha=0.5)
 
-    plotAllPS(powerT, kT, ax[0], color=c, alpha=0.1)
-    plotAllPS(powerP, kP, ax[0], color=c, alpha=0.1)
+    plotAllPS(powerT, kT[0], ax[0], color=c, alpha=0.1)
+    plotAllPS(powerP, kP[0], ax[0], color=c, alpha=0.1)
 
 #ax[0].set_title('spectral resolution {0}km/s\n'.format(spectral_res), y=1.25)
 
-
-ax[0].set_ylabel('$P_F(k) \;\mathrm{[h/Mpc]}^3$', fontsize=12)
-ax[1].set_ylabel(r'$(\overline{P_T} - \overline{P_P})/\sigma_{\bar{P_T} - \bar{P_P}}$', fontsize=11)
-ax[2].set_ylabel('$\sigma_T^2/\sigma_P^2$', fontsize=12)
-ax[2].set_xlabel('k [h/Mpc]', fontsize=12)
+if dimension == '1d':
+    ax[0].set_ylabel('$\mathrm{P_F(k) \; [h/Mpc]}$', fontsize=12)
+else:
+    ax[0].set_ylabel('$\mathrm{3D \; P_F(k) \;[h/Mpc]^3}$', fontsize=12)
+ax[1].set_ylabel(r'$\mathrm{(\overline{P_T} - \overline{P_P})/\sigma_{\bar{P_T} - \bar{P_P}}}$', fontsize=11)
+ax[2].set_ylabel('$\mathrm{\sigma_T^2/\sigma_P^2}$', fontsize=12)
+ax[2].set_xlabel('$\mathrm{k_{\parallel} \; [h/Mpc]}$', fontsize=12)
 
 ax[1].axhline(0, linestyle='--', color='black')
 ax[1].axhline(-3, linestyle='--', color='black', lw=0.5)
@@ -127,7 +146,14 @@ ax[1].axhline(3, linestyle='--', color='black', lw=0.5)
 ax[2].axhline(1, linestyle='--', color='black')
 
 ax[0].set_xlim(0.2, 30)
-ax[0].set_ylim(1e-6, 1e2)
+if dimension == '1d':
+    ax[0].set_ylim(1e-6, 1e0)
+    ax[2].set_xlabel('$\mathrm{k_{\parallel} \; [h/Mpc]}$', fontsize=12)
+
+else:
+    ax[0].set_ylim(1e-6, 1e2)
+    ax[2].set_xlabel('$\mathrm{k \;[h/Mpc]}$', fontsize=12)
+
 ax[1].set_xlim(0.2, 30)
 ax[1].set_ylim(-5, 5)
 ax[2].set_xlim(0.2, 30)
@@ -143,5 +169,5 @@ lgd = [fig.legend(loc=9, bbox_to_anchor=(0.5, 1.02), ncol=4)]
 #legend3 = [lT3, lP3]
 
 fig.tight_layout()
-fig.savefig('varRatio_{0}Mpc_1mubin.pdf'.format(boxsize), additional_artists=lgd,
+fig.savefig('varRatio{0}_{1}Mpc{2}.pdf'.format(dimension, boxsize, plotPostFix), additional_artists=lgd,
 bbox_inches="tight")
